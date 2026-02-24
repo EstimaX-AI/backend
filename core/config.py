4085337120069981291ca5852747d2356bd3e004
mpradeep2005 <pradeep14.3.2005@gmail.com>
2026-02-24 21:44:22 +0530

    feat: creared file structure

---
+++ core/config.py
@@ -0,0 +1,21 @@
+from pydantic_settings import BaseSettings , SettingsConfigDict
+class Setting(BaseSettings):
+
+    db_url : str
+    db_host_name : str
+    db_port : int
+    db_name : str
+    password : str
+
+
+    mq_url : str
+    mq_port : str
+    mq_user : str
+
+    model_config = SettingsConfigDict(
+        env_file=".env.dev",
+        env_file_encoding="utf-8",
+        extra="ignore"
+    )
+setting=Setting()
+print(setting.mq_user)
